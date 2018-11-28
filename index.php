082931776b08a563d24149e0775f9e4ccf31aad2
IgnacioOrt <comosea1122@hotmail.com>
2018-11-28 12:07:31 -0600

    create index

--- index.php
+++ index.php
@@ -3,21 +3,16 @@
 <head>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Uel Uelik</title>
+	<title>M.C. Yolanda Moyao Martínez</title>
 	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link rel="stylesheet" href="css/style.css">
     
 </head>
 
-
-
-<body>
-	<!-- Top menu -->
-	<nav class="navbar navbar-dark fixed-top navbar-expand-md navbar-no-bg">
+<body >
+	<nav class="navbar fixed-top navbar-expand-md">
     	<div class="container">
-	        <a class="navbar-brand" href="index.php">UEL UELIK</a>
+	        <a class="navbar-brand" href="index.php"></a>
         	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 	            <span class="navbar-toggler-icon"></span>
         	</button>
@@ -32,123 +27,12 @@
 	                <li class="nav-item">
                     	<a class="nav-link scroll-link" href="acerca.php">Acerca de</a>
                 	</li>
-                	
             	</ul>
-
-<!--https://bootsnipp.com/snippets/featured/fancy-navbar-login-sign-in-form-->
-            	
-            	<span class="ml-auto navbar-nav">
-            		<li class="nav-item dropdown">
-            			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            				Iniciar sesión
-            			</a>
-            			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" id="login-dp">
-            				<div class="row">
-            					<div class="col-md-12">
-            						<form class="form" role="form" method="post" action="validalogin.php" accept-charset="UTF-8" id="login-nav">
-										<div class="form-group">
-											 <label class="sr-only" for="email">Correo electrónico</label>
-											 <input type="email" class="form-control" name="mail" id="email" placeholder="Correo electrónico" required>
-										</div>
-										<div class="form-group">
-											 <label class="sr-only" for="pass">Contraseña</label>
-											 <input type="password" class="form-control" name="pass" id="pass" placeholder="Contraseña" required>
-										</div>
-										<div class="form-group">
-											 <button type="submit" class="btn btn-primary btn-block">Entrar</button>
-										</div>
-								 </form>
-            					</div>
-            				</div>
-            				<div class="row">
-            					<div class="col-md-12">
-            						<div class="bottom text-center">
-            							¿Aún no estas registrado? <a href="registro.php"><b>Registrarse</b></a>
-            						</div>
-            					</div>
-            				</div>
-            			</div>
-            		</li>
-            	</span>
         	</div>
     	</div>
 	</nav>
-
-                <!--
-                    <div id="captioned-gallery">
-                        <div class="arrows prev"></div>
-
-                        <figure class="slider">
-                            <figure>
-                                <img src="img/inicio1.jpg" >
-                                <figcaption>Oops, Tiré la Tarta de Limón - Massimo Bottura (ITA) </figcaption>
-                            </figure>
-                            <figure>
-                                <img src="img/inicio2.jpg" alt>
-                                <figcaption>Chile en Nogada (MEX) </figcaption>
-                            </figure>
-                            <figure>
-                                <img src="img/inicio3.png" alt>
-                                <figcaption>Mi Cosecha - Dan Barber (USA) </figcaption>
-                            </figure>
-                             <figure>
-                                <img src="img/inicio4.png" alt>
-                                <figcaption>Siete Fuegos - Francis Mallmann (ARG)</figcaption>
-                            </figure>
-                            <figure>
-                                <img src="img/inicio5.png" alt>
-                                <figcaption>Siete Fuegos - Francis Mallmann (ARG)</figcaption>
-                            </figure>
-                            <figure>
-                                <img src="img/inicio6.png" alt>
-                                <figcaption>Siete Fuegos - Francis Mallmann (ARG)</figcaption>
-                            </figure>
-                            </figure>
-                            <div class="arrows next"></div>
-                        </div> -->
-
-
-    
-
-
-    <div id="captioned-gallery">
-            <figure class="slider">
-       
-                <figure>
-                    <img class="mySlides" src="img/inicio1.jpg" style="width:100%">
-                    <figcaption>Oops, Tiré la Tarta de Limón - Massimo Bottura (ITA) </figcaption>
-                </figure>
-
-                <figure>
-                    <img class="mySlides" src="img/inicio2.jpg" style="width:100%">
-                    <figcaption>Chile en Nogada (MEX) </figcaption>
-                </figure>
-
-                <figure>
-                    <img class="mySlides" src="img/Gaggan.png" style="width:100%">
-                    <figcaption>¿Quien Mató al Cordero? - Gaggan Anand (IND) </figcaption>
-                </figure>
-
-                <figure>
-                    <img class="mySlides" src="img/inicio4.jpg" style="width:100%">
-                    <figcaption>Siete Fuegos - Francis Mallmann (ARG) </figcaption>
-                </figure>
-                
-                <figure>
-                    <img class="mySlides" src="img/inicio3.jpg" style="width:100%">
-                    <figcaption>Mi Cosecha - Dan Barber (USA) </figcaption>
-                </figure>
-                
-            </figure>
-    </div>
-
-
-
-
 	<script src="dist/jquery/jquery.slim.min.js"></script>
 	<script src="dist/js/bootstrap.min.js"></script>
 	<script src="dist/popper/umd/popper.min.js"></script>
-
-
 </body>
 </html>
