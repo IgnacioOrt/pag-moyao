676baba205fe46667832d450140f54330a316497
IgnacioOrt <comosea1122@hotmail.com>
2019-01-03 19:46:39 -0600

    Editar paginas

--- admin/paginas.php
+++ admin/paginas.php
@@ -133,53 +133,26 @@
                         <div class="card">
                             <div class="card-header bg-light">Páginas</div>
                             <div class="card-body">
-                                <a href="agregarPagina">Agregar página</a>
+                                <button><a href="agregarPagina">Agregar página</a></button>
+                                
+
                                 <div class="table-responsive">
                                 <table class="table table-striped">
                                     <thead>
                                     <tr>
-                                        <th>ID</th>
-                                        <th>Name</th>
-                                        <th>Sales</th>
-                                        <th>Price</th>
-                                        <th>Discount</th>
+                                        <th>Título</th>
+                                        <th>Fecha</th>
                                     </tr>
                                     </thead>
                                     <tbody>
                                     <tr>
-                                        <td>1</td>
-                                        <td class="text-nowrap">Samsung Galaxy S8</td>
+                                        <td class="text-nowrap">Samsung Galaxy S8<br>
+                                            <a href="edit.php">Editar |</a>
+                                            <a href="">Edición rapida |</a>
+                                            <a href="">Eliminar |</a>
+                                            <a href="">Vista previa |</a>
+                                        </td>
                                         <td>31,589</td>
-                                        <td>$800</td>
-                                        <td>5%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td class="text-nowrap">Google Pixel XL</td>
-                                        <td>99,542</td>
-                                        <td>$750</td>
-                                        <td>3%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td class="text-nowrap">iPhone X</td>
-                                        <td>62,220</td>
-                                        <td>$1,200</td>
-                                        <td>0%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td class="text-nowrap">OnePlus 5T</td>
-                                        <td>50,000</td>
-                                        <td>$650</td>
-                                        <td>5%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>5</td>
-                                        <td class="text-nowrap">Google Nexus 6</td>
-                                        <td>400</td>
-                                        <td>$400</td>
-                                        <td>7%</td>
                                     </tr>
                                     </tbody>
                                 </table>
